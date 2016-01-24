5e8e3926fc273e12d3c62c0f0cc0674f7926e568
user <user@lorbanery>
2016-01-24 15:41:26 +0000

    debug content script isolated from page; run before load

--- data/injected.js
+++ data/injected.js
@@ -1,28 +1,35 @@
+// unsafeWindow: https://blog.mozilla.org/addons/2014/04/10/changes-to-unsafewindow-for-the-add-on-sdk/
 
-(function () {
-	// execute immediately, but in a closure so we can have local variables.
-	if (window.MediaSource !== undefined) {
+unsafeWindow.injected = false;
 
-		// make addSourceBuffer() return a wrapper object
-		var original_function = window.MediaSource.prototype.addSourceBuffer;
-		window.MediaSource.prototype.addSourceBuffer = function () {
-			alert("sourcebuffer");
+// execute immediately, but in a closure so we can have local variables.
+if (unsafeWindow.MediaSource !== undefined) {
 
-			var sourcebuffer = original_function.apply(this, arguments);
 
-			// wrap the appendBuffer function
-			var original_appendbuffer = sourcebuffer.appendBuffer;
-			sourcebuffer.appendBuffer = function () {
-				alert("appendBuffer " + this + ", " + arguments);
-				original_appendbuffer.apply(this, arguments);
-			}
+	// make addSourceBuffer() return a wrapper object
+	var original_function = unsafeWindow.MediaSource.prototype.addSourceBuffer;
+	function replacement_function() {
+		alert("sourcebuffer");
+/*
+		var sourcebuffer = original_function.apply(this, arguments);
 
-			alert("sourcebuffer " + sourcebuffer);
-			return sourcebuffer;
-		};
+		// wrap the appendBuffer function
+		var original_appendbuffer = sourcebuffer.appendBuffer;
+		sourcebuffer.appendBuffer = function () {
+			alert("appendBuffer " + this + ", " + arguments);
+			original_appendbuffer.apply(this, arguments);
+		}
 
-		alert("injected " + window.MediaSource.prototype);
+		alert("sourcebuffer " + sourcebuffer);
+		return sourcebuffer;
+*/
+	};
 
-	}
-})();
+	exportFunction(replacement_function, unsafeWindow, {defineAs: "replacement_function"});
+	unsafeWindow.MediaSource.prototype.addSourceBuffer = unsafeWindow.replacement_function;
 
+	unsafeWindow.injected = true;
+	alert("injected " + unsafeWindow.MediaSource.prototype);
+
+
+}
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var self = require('sdk/self');
 
 var pageMod = require("sdk/page-mod");
 pageMod.PageMod({
-  include: /.*(example\.(com|org)|youtube\.com).*/,
-  contentScriptFile: self.data.url("injected.js")
+	// include: /.*(example\.(com|org)|youtube\.com).*/,
+	include: "*",
+	contentScriptFile: self.data.url("injected.js"),
+	contentScriptWhen: "start",
+	// onAttach: a function which adds a listener for postMessage
+	attachTo: ["existing", "top", "frame"]
 });
